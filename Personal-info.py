822b834c34e98b25c6d9ea777f6227b955bbfbc3
liamoral <82429876+liamoral@users.noreply.github.com>
2021-04-13 12:03:59 +0800

    Add files via upload

---
+++ Personal-info.py
@@ -0,0 +1,6 @@
+def personal_details():
+    name, age = "Liam", 21
+    address = "Dasmarinas City, Cavite, Philippines"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+personal_details()
